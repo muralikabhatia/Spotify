3b5a229ddcf186b57b71262f3efc7316bf034dfc
muralikabhatia <muralikabhatia2511@gmail.com>
2022-07-23 23:31:55 +0530

    play/pause update

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let songIndex=0;
 let audioElement=new Audio('1.mp3');
 let masterPlay= document.getElementById('masterPlay');
 let myProgressBar=document.getElementById('myProgressBar');
+let gif=document.getElementById('gif');
 
 
 let songs=[
@@ -24,16 +25,21 @@ masterPlay.addEventListener('click', ()=>{
     if(audioElement.paused || audioElement.currentTime<=0){
         audioElement.play();
         masterPlay.classList.remove('fa-play-circle');
-        masterPlay.classList.add('fa-pause-circle')
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity=1;
     }
     else{
         audioElement.pause();
         masterPlay.classList.remove('fa-pause-circle');
         masterPlay.classList.add('fa-play-circle')
+        gif.style.opacity=0;
     }
 })
 //Listen to events
-document.addEventListener('timeupdate', ()=>{
+audioElement.addEventListener('timeupdate', ()=>{
     console.log('timeupdate');
     //Update Seekbar
+    progress=parseInt((audioElement.currentTime/audioElement.duration)*100);
+    console.log(progress);
+    myProgressBar.value= progress;
 })
