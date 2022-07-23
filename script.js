6788918f93e70aa9e872785ac9009e5c677ae7cc
muralikabhatia <muralikabhatia2511@gmail.com>
2022-07-23 23:00:58 +0530

    some edit

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 console.log("Welcome to Spotify");
+
+//Intializing variables
+let songIndex=0;
 let audioElement=new Audio('1.mp3');
+let masterPlay= document.getElementById('masterPlay');
+let myProgressBar=document.getElementById('myProgressBar');
+
+let songs=[
+    {songName: "Ranjha", filePath:"song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Ranjha", filePath:"song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Ranjha", filePath:"song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Ranjha", filePath:"song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Ranjha", filePath:"song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Ranjha", filePath:"song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Ranjha", filePath:"song/1.mp3", coverPath: "covers/1.jpg"},
+]
+
 // audioElement.play();
+
+//Listen to events
+document.addEventListener('time');
